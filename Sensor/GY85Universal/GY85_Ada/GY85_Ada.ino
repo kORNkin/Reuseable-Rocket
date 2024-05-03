d236c4b4af1957873d0ca4ff01c6622e179d36b7
usernametoon_ <cartoonpiboon@gmail.com>
2024-05-03 16:18:59 +0700

    add set origin

--- Sensor/GY85Universal/GY85_Ada/GY85_Ada.ino
+++ Sensor/GY85Universal/GY85_Ada/GY85_Ada.ino
@@ -1,27 +1,31 @@
-#include <Adafruit_AHRS.h>
 #include <IMUGY85.h>
 
 
 IMUGY85 imu;
 double ax, ay, az, gx, gy, gz, mx, my, mz, roll, pitch, yaw;
-
-uint32_t timestamp;
+double* Offset;
 
 void setup()
 {
   Serial.begin(9600);
   imu.init();
+  Offset = IMUOffset();
+  Serial.print(Offset[0]); Serial.print(", ");
+  Serial.print(Offset[1]); Serial.print(", ");
+  Serial.print(Offset[2]); Serial.println(" ");
+  
+  
 }
 
 void loop()
 {
   imu.update();
   
-  // printAccel();
-  // printGyro();
-  printMagneto();
-  // printRollPitchYaw();
-  
+  // // printAccel();
+  // // printGyro();
+  // // printMagneto();
+  printRollPitchYaw(Offset);
+
   delay(10);
 }
 
@@ -31,7 +35,6 @@ void printAccel()
   Serial.print(ax);Serial.print("\t");
   Serial.print(ay);Serial.print("\t");
   Serial.print(az);Serial.print("\t");
-  Serial.println();
 }
 
 void printGyro()
@@ -40,7 +43,6 @@ void printGyro()
   Serial.print(gx);Serial.print("\t");
   Serial.print(gy);Serial.print("\t");
   Serial.print(gz);Serial.print("\t");
-  Serial.println();
 }
 
 void printMagneto()
@@ -52,14 +54,42 @@ void printMagneto()
   Serial.println();
 }
 
-void printRollPitchYaw()
+void printRollPitchYaw(double* offset)
 {
-  roll = imu.getRoll();
-  pitch = imu.getPitch();
-  yaw = imu.getYaw();
+  roll = imu.getRoll() - offset[0];
+  pitch = imu.getPitch() - offset[1];
+  yaw = imu.getYaw() - offset[2];
+  yaw = UseableYaw(yaw);
   Serial.print(pitch);Serial.print("\t");
   Serial.print(roll);Serial.print("\t");
   Serial.print(yaw);Serial.print("\t");
   Serial.println();
 }
 
+double UseableYaw(double yaw){
+  if(yaw > 180) return yaw - 360;
+  return yaw; 
+}
+//wait to add timer
+double* IMUOffset(){
+  unsigned int timer;
+  timer = millis() + 10000;
+  while(millis() <= timer){
+    imu.update();
+    delay(10);
+  }
+  int c = 0;
+  static double Er[3];
+  double rollEr,pitchEr,yawEr ;
+  while(c < 2000){
+    imu.update();
+    rollEr += imu.getRoll();
+    pitchEr += imu.getPitch();
+    yawEr += imu.getYaw();
+    c++;
+  }
+  Er[0] = rollEr / c;
+  Er[1] = pitchEr / c;
+  Er[2] = yawEr / c;
+  return Er;
+}
